91888ec005f54c53009a0603d22f67494784c896
Mark <mborden@uwo.ca>
2015-06-12 15:59:27 -0400

    Add name field to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :username, :length => { :maximum => 10 }
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150612160322) do
+ActiveRecord::Schema.define(version: 20150612194252) do
 
   create_table "profiles", force: :cascade do |t|
     t.text     "description"
@@ -40,9 +40,11 @@ ActiveRecord::Schema.define(version: 20150612160322) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
