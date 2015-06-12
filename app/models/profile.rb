06e298a6dbeafbe37d7817f0969fe319b2e2fd65
Mark <mborden@uwo.ca>
2015-06-12 19:12:35 -0400

    created user relationship

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,8 @@
 class Profile < ActiveRecord::Base
 	mount_uploader :image, ImageUploader
+
+	validates :description, :steam, :mmr, presence: true
+	validates :mmr, numericality: { greater_than: 0, less_than: 9999 }
+
+	belongs_to :user
 end
