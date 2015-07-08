62b436f1ec950fc19d7b1e23b4c852c2786400b9
Mark <mborden@uwo.ca>
2015-07-08 12:58:00 -0400

    fixed styling

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,7 +4,7 @@ class Profile < ActiveRecord::Base
 	validates :description, :mmr, presence: true
 	validates :mmr, numericality: { greater_than: 0, less_than: 9999 }
 	validates :description, length: {minimum: 100, maximum: 1000}
-	validates :image, file_size: { less_than_or_equal_to: 500.kilobytes }, presence: true
+	validates :image, file_size: { less_than_or_equal_to: 500.kilobytes }
 
 	
 	belongs_to :language, :class_name => Language, :foreign_key => :language_id
