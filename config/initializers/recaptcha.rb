65ae0dff43b1c572d17df6b5b1c4a33bb5d6e6bb
Mark <mborden@uwo.ca>
2015-07-10 16:12:06 -0400

    Working captcha

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,5 +1,5 @@
 Recaptcha.configure do |config|
-  config.public_key  = ENV['CAPTCHA_PUBLIC_KEY'],
-  config.private_key = ENV['CAPTCHA_PRIVATE_KEY'],
+  config.public_key  = ENV['CAPTCHA_PUBLIC_KEY']
+  config.private_key = ENV['CAPTCHA_PRIVATE_KEY']
   config.use_ssl_by_default = true
 end
